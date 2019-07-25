3a2131881d2e8890dd9ce31f9402c299baaba3d1
Rondinele Guimaraes <rondineleg@edu.univai.br>
2019-07-25 12:38:44 -0300

    Criando Migrations e view Funcionarios

---
+++ src/AppSignalR.Web/Data/AppSignalRWebContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AppSignalR.Web.Models;
+
+    public class AppSignalRWebContext : DbContext
+    {
+        public AppSignalRWebContext (DbContextOptions<AppSignalRWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AppSignalR.Web.Models.Funcionario> Funcionario { get; set; }
+    }
--- src/AppSignalR.Web/Startup.cs
+++ src/AppSignalR.Web/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace AppSignalR.Web
 {
@@ -33,6 +34,9 @@ namespace AppSignalR.Web
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<AppSignalRWebContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("AppSignalRWebContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
